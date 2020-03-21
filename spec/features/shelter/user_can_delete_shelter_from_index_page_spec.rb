88684b494ce16074a325f76b8518f919f26e445a
Daniel Atkinson <atkinson.daniel7@gmail.com>
2020-03-21 15:25:36 -0600

    Added delete shelter link to shelter index page

---
+++ spec/features/shelter/user_can_delete_shelter_from_index_page_spec.rb
@@ -0,0 +1,25 @@
+require 'rails_helper'
+
+RSpec.describe "On shelter index page user can click delete shelter link", type: :feature do
+  it "deletes the shelter" do
+    shelter1 = Shelter.create(name: "Humane Society of Boulder Valley",
+                              address: "2323 55th St",
+                              city: "Boulder",
+                              state: "CO",
+                              zip: "80301")
+    shelter2 = Shelter.create(name: "Longmont Humane Society",
+                              address: "9595 Nelson Rd",
+                              city: "Longmont",
+                              state: "CO",
+                              zip: "80501")
+
+
+    delete_shelters_path = "/shelters/#{shelter1.id}"
+    visit '/shelters'
+    find("a[href='#{delete_shelters_path}']").click
+
+    expect(page).to have_current_path("/shelters")
+    expect(page).to have_content(shelter2.name)
+    expect(page).to_not have_content(shelter1.name)
+  end
+end
